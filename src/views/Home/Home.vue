6f0c67550986dfb5ae41e15e10782cc016e168e9
kalipoison <1174616074@qq.com>
2021-12-01 01:26:09 +0800

    echarts 折线图 柱状图 饼状图

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -44,10 +44,16 @@
                     </div>
                 </el-card>
             </div>
-            <el-card shadow="hover" style="height: 200px"></el-card>
+            <el-card shadow="hover" style="height: 280px">
+                <div style="height: 280px; width: 100%" ref="OrderEchart"></div>
+            </el-card>
             <div class="graph">
-                <el-card shadow="hover" style="height: 280px"></el-card>
-                <el-card shadow="hover" style="height: 280px"></el-card>
+                <el-card shadow="hover" style="height: 280px">
+                    <div style="height: 280px; width: 100%" ref="UserEchart"></div>
+                </el-card>
+                <el-card shadow="hover" style="height: 280px">
+                    <div style="height: 280px; width: 100%" ref="VideoEchart"></div>
+                </el-card>
             </div>
         </el-col>
     </el-row>
@@ -55,7 +61,8 @@
 
 <script>
 
-import { getHome } from '../../api/data'
+import { getHome } from '../../api/data';
+import * as echarts from 'echarts';
 
 export default {
     data() {
@@ -93,7 +100,7 @@ export default {
                 monthBuy : '月销量',
                 totalBuy : '总销量',
             },
-            countData: [{
+            countData : [{
                 name: "今日支付订单",
                 value: 1234,
                 icon: "success",
@@ -129,12 +136,146 @@ export default {
                 icon: "s-goods",
                 color: "#5ab1ef",
             }],
+            echartsData : {
+                order : {
+                    legend: {
+                        // 图例文字颜色
+                        textStyle: {
+                            color: "#333",
+                        },
+                    },
+                    grid: {
+                        left: "20%",
+                    },
+                    // 提示框
+                    tooltip: {
+                        trigger: "axis",
+                    },
+                    xAxis: {
+                        type: "category", // 类目轴
+                        data: [],
+                        axisLine: {
+                            lineStyle: {
+                                color: "#17b3a3",
+                            },
+                        },
+                        axisLabel: {
+                            interval: 0,
+                            color: "#333",
+                        },
+                    },
+                    yAxis: [
+                        {
+                            type: "value",
+                            axisLine: {
+                                lineStyle: {
+                                    color: "#17b3a3",
+                                },
+                            },
+                        },
+                    ],
+                    color: ["#2ec7c9", "#b6a2de", "#5ab1ef", "#ffb980", "#d87a80", "#8d98b3"],
+                    series: [],
+                },
+                user : {
+                    legend: {
+                        // 图例文字颜色
+                        textStyle: {
+                            color: "#333",
+                        },
+                    },
+                    grid: {
+                        left: "20%",
+                    },
+                    // 提示框
+                    tooltip: {
+                        trigger: "axis",
+                    },
+                    xAxis: {
+                        type: "category", // 类目轴
+                        data: [],
+                        axisLine: {
+                            lineStyle: {
+                                color: "#17b3a3",
+                            },
+                        },
+                        axisLabel: {
+                            interval: 0,
+                            color: "#333",
+                        },
+                    },
+                    yAxis: [
+                        {
+                            type: "value",
+                            axisLine: {
+                                lineStyle: {
+                                    color: "#17b3a3",
+                                },
+                            },
+                        },
+                    ],
+                    color: ["#2ec7c9", "#b6a2de", "#5ab1ef", "#ffb980", "#d87a80", "#8d98b3"],
+                    series: [],
+                },
+                video : {
+                    tooltip: {
+                        trigger: "item",
+                    },
+                    color: [
+                        "#0f78f4",
+                        "#dd536b",
+                        "#9462e5",
+                        "#a6a6a6",
+                        "#e1bb22",
+                        "#39c362",
+                        "#3ed1cf",
+                    ],
+                    series: [],
+                },
+            },
         }
     },
     methods : {
         getTableData () {
             getHome().then((res) => {
                 console.info("res", res.data);
+
+                // 折线图的展示
+                const order = res.data.orderData;
+                this.echartsData.xAxis = order;
+                let keyArray = Object.keys(order.data[0]);
+                keyArray.forEach((key)=>{
+                    this.echartsData.order.series.push({
+                        name : key,
+                        data : order.data.map((item) => item[key]),
+                        type : 'line',
+                    });
+                });
+                const myEchartsOrder = echarts.init(this.$refs.OrderEchart);
+                myEchartsOrder.setOption(this.echartsData.order);
+
+                // 柱状图的展示
+                this.echartsData.user.xAxis.data = res.data.userData.map((item) => item.date);
+                this.echartsData.user.series.push({
+                    name : '新增用户',
+                    data : res.data.userData.map((item) => item.new),
+                    type : 'bar',
+                });
+                this.echartsData.user.series.push({
+                    name : '活跃用户',
+                    data : res.data.userData.map((item) => item.active),
+                    type : 'bar',
+                });
+                const myEchartsUser = echarts.init(this.$refs.UserEchart);
+                myEchartsUser.setOption(this.echartsData.user);
+
+                // 饼状图的展示
+                this.echartsData.video.series.push({
+                    data : res.data.videoData,
+                    type : 'pie',
+                });
+                const myEchartsVideo = echarts.init(this.$refs.VideoEchart);
+                myEchartsVideo.setOption(this.echartsData.video);
             });
         },
     },
