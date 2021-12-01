a3460e9d7ec0e28bb1ee39db79afd481ba0390f5
kalipoison <1174616074@qq.com>
2021-12-01 22:54:06 +0800

    登录，用户管理数据展示

--- src/api/mock.js
+++ src/api/mock.js
@@ -1,5 +1,12 @@
 import Mock from 'mockjs'
 
 import homeApi from './mockServerData/home'
+import userApi from './mockServerData/user'
+import permissionApi from './mockServerData/permission'
+
 
 Mock.mock('/api/home/getData', homeApi.getStatisticalData)
+
+Mock.mock(/api\/user\/getUser/, 'get', userApi.getUserList)
+// 权限相关
+Mock.mock(/api\/permission\/getMenu/, 'post', permissionApi.getMenu)
